78db15671d4374eb17ca0cba8d15a0efb545a82c
Magnus Haug <magnus@stack.no>
2013-04-15 23:04:53 +0200

    Support both new Interface({}) and Interface.extend({}), support implements(foo, bar, baz)

--- backbone-interfaces.js
+++ backbone-interfaces.js
@@ -1,21 +1,19 @@
 (function(Backbone) {
 
-	Backbone.Model.implements = Backbone.Collection.implements = Backbone.Router.implements = Backbone.View.implements = function(interface) {
-		var interfaces = this._interfaces || [];
-		interfaces.push(interface);
-
-		this._interfaces = interfaces;
-		return this;
+    // When calling .implements() add the interfaces to a list of initialize-time checks to be done.
+	Backbone.Model.implements = Backbone.Collection.implements = Backbone.Router.implements = Backbone.View.implements = function() {
+        Array.prototype.push.apply(this._interfaces = this._interfaces || [], arguments);
+        return this;
 	};
 
+    // When instantiating an object, perform implementation checks.
 	Backbone.Model = Backbone.Model.extend({
 		initialize: function(){
-			var interfaces = this.__proto__.constructor._interfaces; // TODO get from all interfaces;
-
-			var base = this;
-			_.each(interfaces, 		function(interface){
-				_.each(_.functions(interface), 	function(fname){
-					if ( ! _.isFunction(base[fname])){
+			var clazz = this,
+                interfaces = clazz.__proto__.constructor._interfaces;
+			_.each(interfaces, function(interface){
+				_.each(_.functions(interface), function(fname){
+					if ( ! _.isFunction(clazz[fname])){
 						throw "Did not correctly implement '" + fname + "()' from an implemented interface!";
 					}
 				});
@@ -23,8 +21,14 @@
 		}
 	});
 
-	Backbone.Interface = function(attrs){
-		_.extend(this.__proto__, attrs);
-		return this;
-	};
+    // Support `new Backbone.Interface({foo: function(){}})`
+    Backbone.Interface = function(attrs){
+        _.extend(this.__proto__, attrs);
+        return this;
+    };
+
+    // Support `Backbone.Interface.extend({foo: function(){}})`
+    Backbone.Interface.extend = function(){
+        return _.extend.apply(this.__proto__, arguments);
+    };
 })(Backbone);
