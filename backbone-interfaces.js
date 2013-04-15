6b86861fee4225cdbc47000c407be512473549d9
Magnus Haug <magnus@stack.no>
2013-04-15 22:15:17 +0200

    First try, probably won't support interfaces in inherited objects

---
+++ backbone-interfaces.js
@@ -0,0 +1,30 @@
+(function(Backbone) {
+
+	Backbone.Model.implements = Backbone.Collection.implements = Backbone.Router.implements = Backbone.View.implements = function(interface) {
+		var interfaces = this._interfaces || [];
+		interfaces.push(interface);
+
+		this._interfaces = interfaces;
+		return this;
+	};
+
+	Backbone.Model = Backbone.Model.extend({
+		initialize: function(){
+			var interfaces = this.__proto__.constructor._interfaces; // TODO get from all interfaces;
+
+			var base = this;
+			_.each(interfaces, 		function(interface){
+				_.each(_.functions(interface), 	function(fname){
+					if ( ! _.isFunction(base.__proto__[fname])){
+						throw "Did not correctly implement '" + fname + "()' from an implemented interface!";
+					}
+				});
+			});
+		}
+	});
+
+	Backbone.Interface = function(attrs){
+		_.extend(this.__proto__, attrs);
+		return this;
+	};
+})(Backbone);
